cba5140e99b195283e8b24c3500461807f8ded82
Archie <archielola123@gmail.com>
2025-08-20 12:58:07 +0800

    Update upcoming.js

--- functions/upcoming.js
+++ functions/upcoming.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
   try {
-    const res = await fetch("https://lfapurpose.ghost.io/ghost/api/content/posts/?key=YOUR_CONTENT_API_KEY&filter=visibility:public+status:scheduled&limit=3&order=published_at%20asc");
+    const res = await fetch("https://lfapurpose.ghost.io/ghost/api/content/posts/?key=7ff72edc7875fc32f9a4337567&filter=visibility:public+status:scheduled&limit=3&order=published_at%20asc");
     const data = await res.json();
 
     return {
@@ -14,3 +14,4 @@ export async function handler(event, context) {
     };
   }
 }
+
